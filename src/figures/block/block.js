c456d1d5c37f84b8587ca162f128d14c4f48007a
anastasiya.berezhnaya <anastasiya.berezhnaya@wilix.org>
2021-05-11 17:08:53 +0300

    init commit

---
+++ src/figures/block/block.js
@@ -0,0 +1,14 @@
+import "./block.scss";
+
+export const Test = () => {
+  return (
+    <div className="block">
+      <div className="back"></div>
+      <div className="front"></div>
+      <div className="top"></div>
+      <div className="left"></div>
+      <div className="right"></div>
+      <div className="bottom"></div>
+    </div>
+  );
+};
